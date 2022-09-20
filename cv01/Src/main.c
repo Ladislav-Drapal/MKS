432f4f54d9ccaaa19be20f5cf5b1231b5f09cb2d
Ladislav Drapal <xdrapa09@vutbr.cz>
2022-09-20 14:30:02 +0200

    blikani LED

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -16,7 +16,8 @@
  ******************************************************************************
  */
 
-#include <stdint.h>
+/** #include <stdint.h>
+#include <stm32f0xx.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -24,6 +25,37 @@
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+	for(;;) {
+		GPIOA->ODR ^= (1<<5); // toggle
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+	}
+}
+
+*/
+#include <stdint.h>
+#include <stm32f0xx.h>
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+int main(void)
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+    uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
+
+	for(;;) {
+
+		for (i=0; i<32; i++){
+			if(pole[i]) GPIOA->BSRR = (1<<5); // set
+		        else GPIOA->BRR = (1<<5);
+		}
+
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+	}
 }
