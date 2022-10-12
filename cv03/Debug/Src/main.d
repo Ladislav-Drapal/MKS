69bbe047f2dd85f39bf7b4d9cd5ce9500e790a30
Ladislav Drapal <xdrapa09@vutbr.cz>
2022-10-12 15:43:36 +0200

    cv3a

--- cv03/Debug/Src/main.d
+++ cv03/Debug/Src/main.d
@@ -1,13 +1,14 @@
-Src/main.o: ../Src/main.c \
- C:/Temp/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
- C:/Temp/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
- C:/Temp/MKS/cv02/CMSIS/Include/core_cm0.h \
- C:/Temp/MKS/cv02/CMSIS/Include/core_cmInstr.h \
- C:/Temp/MKS/cv02/CMSIS/Include/core_cmFunc.h \
- C:/Temp/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
-C:/Temp/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
-C:/Temp/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
-C:/Temp/MKS/cv02/CMSIS/Include/core_cm0.h:
-C:/Temp/MKS/cv02/CMSIS/Include/core_cmInstr.h:
-C:/Temp/MKS/cv02/CMSIS/Include/core_cmFunc.h:
-C:/Temp/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+Src/main.o: ../Src/main.c ../Src/sct.h \
+ C:/Temp/MKS/cv03/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Temp/MKS/cv03/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/Temp/MKS/cv03/CMSIS/Include/core_cm0.h \
+ C:/Temp/MKS/cv03/CMSIS/Include/core_cmInstr.h \
+ C:/Temp/MKS/cv03/CMSIS/Include/core_cmFunc.h \
+ C:/Temp/MKS/cv03/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+../Src/sct.h:
+C:/Temp/MKS/cv03/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+C:/Temp/MKS/cv03/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+C:/Temp/MKS/cv03/CMSIS/Include/core_cm0.h:
+C:/Temp/MKS/cv03/CMSIS/Include/core_cmInstr.h:
+C:/Temp/MKS/cv03/CMSIS/Include/core_cmFunc.h:
+C:/Temp/MKS/cv03/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
--- cv03/Debug/Src/subdir.mk
+++ cv03/Debug/Src/subdir.mk
@@ -6,18 +6,21 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Src/main.c \
+../Src/sct.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c \
 ../Src/system_stm32f0xx.c 
 
 OBJS += \
 ./Src/main.o \
+./Src/sct.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o \
 ./Src/system_stm32f0xx.o 
 
 C_DEPS += \
 ./Src/main.d \
+./Src/sct.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d \
 ./Src/system_stm32f0xx.d 
@@ -25,12 +28,12 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F030x8 -DSTM32F030R8Tx -DSTM32 -DSTM32F0 -DNUCLEO_F030R8 -c -I"C:/Temp/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include" -I"C:/Temp/MKS/cv02/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F030x8 -DSTM32F030R8Tx -DSTM32 -DSTM32F0 -DNUCLEO_F030R8 -c -I"C:/Temp/MKS/cv03/CMSIS/Device/ST/STM32F0xx/Include" -I"C:/Temp/MKS/cv03/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32f0xx.d ./Src/system_stm32f0xx.o ./Src/system_stm32f0xx.su
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/main.su ./Src/sct.d ./Src/sct.o ./Src/sct.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32f0xx.d ./Src/system_stm32f0xx.o ./Src/system_stm32f0xx.su
 
 .PHONY: clean-Src
 
